7f0306002749ef86ec040947708a4c07f9f805fb
Gabriel Candal <gcc@platforme.com>
2020-01-31 09:13:18 +0000

    First

---
+++ android/app/src/main/java/com/platforme/ripe_robin/MainActivity.java
@@ -0,0 +1,15 @@
+package com.platforme.ripe_components_react_native_app;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "ripe-components-react-native-app";
+    }
+}
