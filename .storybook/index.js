c653ced132646c88b4d6b28974986776dab26f07
Gabriel Candal <gcc@platforme.com>
2020-02-05 19:40:26 +0000

    Fix storybook init

--- .storybook/index.js
+++ .storybook/index.js
@@ -9,8 +9,7 @@ configure(() => {
 }, module);
 
 const StorybookUIRoot = getStorybookUI({
-    // Uncomment once https://github.com/storybookjs/storybook/issues/9294 is fixed
-    // asyncStorage: require("@react-native-community/async-storage").AsyncStorage
+    asyncStorage: require("@react-native-community/async-storage").AsyncStorage
 });
 AppRegistry.registerComponent("RIPE Components React Native", () => StorybookUIRoot);
 
