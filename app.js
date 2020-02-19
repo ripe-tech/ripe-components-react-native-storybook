a8480217a27181f522ce70fc137e8fec035e7cf0
joamag <joamag@gmail.com>
2020-02-19 15:39:12 +0000

    New settings set

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+import { Platform } from "react-native";
+
+if (Platform.OS === "android") {
+    if (UIManager.setLayoutAnimationEnabledExperimental) {
+        UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+}
+
 export default from "./storybook";
