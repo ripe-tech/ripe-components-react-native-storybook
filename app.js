7edab63551cf5229fec3aef86b5fcaf162d6ba9a
joamag <joamag@gmail.com>
2020-02-19 15:39:50 +0000

    Small code fix

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { Platform } from "react-native";
+import { Platform, UIManager } from "react-native";
 
 if (Platform.OS === "android") {
     if (UIManager.setLayoutAnimationEnabledExperimental) {
